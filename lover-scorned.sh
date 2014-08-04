0c7386445e1383f8ce9addca3f82a3649f3c3354
Sandy Maguire <sandy@sandymaguire.me>
2014-08-04 03:29:22 -0400

    it feels so good to be loved

--- lover-scorned.sh
+++ lover-scorned.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-while :
-do
+#while :
+#do
     date > DATE_ME_HARDER
     git add .
     git commit -m "it feels so good to be loved"
     git push
-    sleep 5m
-done
+    #sleep 5m
+#done
 
